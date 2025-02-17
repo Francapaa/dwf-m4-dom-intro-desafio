548139db3f047bbcdd0901b0f5d4e79aec56d2d7
franciscocaparruva@gmail.com <franciscocaparruva@gmail.com>
2025-02-17 16:52:42 -0300

    Done

--- index.js
+++ index.js
@@ -21,10 +21,26 @@ const cosasQueAprendimos = [
   },
   {
     tema: "dom",
-    class: "special",
+    class: "",
   },
 ];
 
-function main() {}
+function main() {
+    const listaTotal = document.querySelector(".lista"); 
+    
+    console.log(listaTotal);   
+    while (listaTotal.firstChild) {
+      listaTotal.removeChild(listaTotal.firstChild);
+  }
+
+    for (let i = 0; i<cosasQueAprendimos.length; i++){
+        let newLi = document.createElement("li"); 
+        newLi.textContent = cosasQueAprendimos[i].tema;
+        if (cosasQueAprendimos[i].class){
+          newLi.classList.add(cosasQueAprendimos[i].class);
+        }
+        listaTotal.appendChild(newLi); 
+      }
+}
 
 main();
